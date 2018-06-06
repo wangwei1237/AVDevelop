bab11034e0ffef85b3a37f4839eadee5ae5a398e
Wang Wei <wangwei1237@gmail.com>
2018-06-06 10:48:56 +0800

    修改文件名

---
+++ face_detect/main.py
@@ -0,0 +1,24 @@
+#!encoding=utf-8
+
+import apiutil
+from PIL import Image
+import json
+
+AppID  = '1106954466'
+AppKey = '5u6ehLVCgxhznKhX'
+
+ai_obj = apiutil.AiPlat(AppID, AppKey)
+with open('1.jpg', 'rb') as bin_data:
+    image_data = bin_data.read()
+
+rsp = ai_obj.getFaceInfo(image_data, 0)
+if rsp['ret'] == 0:
+    beauty = 0
+    
+    for face in rsp['data']['face_list']:
+        #print(face)
+        face_area = (face['x'], face['y'], face['x'] + face['width'], face['y'] + face['height'])
+        #print(face_area)
+        img = Image.open("1.png")
+        cropped_img = img.crop(face_area).convert('RGB')
+        cropped_img.save('data/' + face['face_id'] + '.png')
